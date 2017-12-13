5a8a4728e0c9174e249192095a09afe8657fb8dc
Natko Biscan <nbiscan7@gmail.com>
2017-12-13 11:22:33 +0100

    search elements

--- src/components/user/SearchEquipment/SearchEquipment.js
+++ src/components/user/SearchEquipment/SearchEquipment.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Search, Dropdown } from 'semantic-ui-react';
+import './SearchEquipment.css';
 
 class SearchEquipment extends React.Component {
+
 	render() {
-		return(
+		return (
 			<div>
 				SEARCH EQUIPMENT
+				<br />
+				<Dropdown className="dropdown" placeholder="Search by:" selection options={[{ text: 'id', value: 'id' }, { text: 'name', value: 'name' }, { text: 'type', value: 'type' }]} />
+				<Search className="search" />
 			</div>
+
 		);
 	}
 }
