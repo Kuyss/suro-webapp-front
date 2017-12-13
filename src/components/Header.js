da5ada1d8b88ea9c40437c8e21018509347d294c
Natko Biscan <nbiscan7@gmail.com>
2017-12-13 09:17:29 +0100

    Header tabs for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,44 +7,61 @@ import { Icon, Menu } from 'semantic-ui-react'
 export default class Header extends Component {
 	state = { activeItem: 'home', role: "ADMIN" }
 
-  	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
 	render() {
 		const { activeItem, role } = this.state
 
-		return(
+		return (
 			<div>
 				<Menu icon='labeled'>
 					<Link to="/">
-			          	<Menu.Item as="span" name='home' active={activeItem === 'home'} onClick={this.handleItemClick} >
-			          		<Icon name="home" />
-			          		Home
+						<Menu.Item as="span" name='home' active={activeItem === 'home'} onClick={this.handleItemClick} >
+							<Icon name="home" />
+							Home
 			          	</Menu.Item>
-			        </Link>
-				  {
-				  	role === roles.ADMIN &&
-				  		<Menu.Menu>
-						  	<Link to="/user_management">
-					          	<Menu.Item as="span" name='user management' active={activeItem === 'user management'} onClick={this.handleItemClick} >
-					          		<Icon name="user" />
-			          				User Management
+					</Link>
+					{
+						role === roles.ADMIN &&
+						<Menu.Menu>
+							<Link to="/user_management">
+								<Menu.Item as="span" name='user management' active={activeItem === 'user management'} onClick={this.handleItemClick} >
+									<Icon name="user" />
+									User Management
 					          	</Menu.Item>
-					        </Link>
-					        <Link to="/equipment_management">
-					          	<Menu.Item as="span" name='equipment management' active={activeItem === 'equipment management'} onClick={this.handleItemClick} >
-						          	<Icon name="archive" />
-				          				Equipment Management
+							</Link>
+							<Link to="/equipment_management">
+								<Menu.Item as="span" name='equipment management' active={activeItem === 'equipment management'} onClick={this.handleItemClick} >
+									<Icon name="archive" />
+									Equipment Management
 					          	</Menu.Item>
-					        </Link>
-				        </Menu.Menu>
-				  }
-				  {
-				  	role === roles.USER &&
-					  	<Menu.Menu>
-
-					  	</Menu.Menu>
-				  }
-		        </Menu>
+							</Link>
+						</Menu.Menu>
+					}
+					{
+						role === roles.USER &&
+						<Menu.Menu>
+							<Link to="/search_equipment">
+								<Menu.Item as="span" name='search equipment' active={activeItem === 'search equipment'} onClick={this.handleItemClick}>
+									<Icon name="search" />
+									Search Equipment
+								</Menu.Item>
+							</Link>
+							<Link to="/active_reservations">
+								<Menu.Item as="span" name='active reservations' active={activeItem === 'active reservations'} onClick={this.handleItemClick}>
+									<Icon name="find" />
+									Active Reservations
+								</Menu.Item>
+							</Link>
+							<Link to="/history">
+								<Menu.Item as="span" name='history' active={activeItem === 'history'} onClick={this.handleItemClick}>
+									<Icon name="history" />
+									History of Reservations
+								</Menu.Item>
+							</Link>
+						</Menu.Menu>
+					}
+				</Menu>
 			</div>
 		);
 	}
