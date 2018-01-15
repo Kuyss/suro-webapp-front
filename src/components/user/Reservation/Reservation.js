f4250adeff7d001e35c2f4373857d9fff6c75205
Natko Biscan <nbiscan7@gmail.com>
2018-01-15 21:28:30 +0100

    approved check

--- src/components/user/Reservation/Reservation.js
+++ src/components/user/Reservation/Reservation.js
@@ -4,6 +4,7 @@ import './Reservation.css';
 import Image from 'semantic-ui-react/dist/commonjs/elements/Image/Image';
 import ItemList from '../ItemList/ItemList';
 import HistoryList from '../HistoryList/HistoryList';
+import Label from 'semantic-ui-react/dist/commonjs/elements/Label/Label';
 
 export default class Reservation extends React.Component {
 
@@ -35,27 +36,32 @@ export default class Reservation extends React.Component {
                             <Item.Content >
 
                                 <HistoryList items={this.props.reservation.items} />
-                                <Item.Description>Start date: {this.props.reservation.start_date}</Item.Description>
-                                <Item.Description>Return date: {this.props.reservation.return_date}</Item.Description>
-                                <br /> <br /> <br /> <br />
-                                {!this.props.his&&<div className="grey">
-                                   
-                                    <h3>New return date</h3>
-                                    <div className="ui calendar" id="example1">
-                                        <div className="ui input left icon">
-                                            <i className="calendar icon"></i>
-                                            <input type="text" placeholder="returndate" ref={(input) => {
-                                                this.end = input;
-                                            }} />
+                                {(this.props.reservation.status.id !== 2) && <div>
+                                    <br />
+                                    <Item.Description>Start date: {this.props.reservation.start_date}</Item.Description>
+                                    <Item.Description>Return date: {this.props.reservation.return_date}</Item.Description>
+                                    <br /> <br /> <br /> <br />
+                                    {!this.props.his && <div className="grey">
+
+                                        <h3>New return date</h3>
+                                        <div className="ui calendar" id="example1">
+                                            <div className="ui input left icon">
+                                                <i className="calendar icon"></i>
+                                                <input type="text" placeholder="returndate" ref={(input) => {
+                                                    this.end = input;
+                                                }} />
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    
-                                    {!this.props.his && <Button floated='right' onClick={() => this.props.ext(this.props.reservation.id, this.end.value)}>Extend reservation</Button>}
-                                    {!this.props.his && <Button floated='right' onClick={() => this.props.del(this.props.reservation.id)}>Delete reservation</Button>}
 
+                                        {!this.props.his && <Button floated='right' onClick={() => this.props.ext(this.props.reservation.id, this.end.value)}>Extend reservation</Button>}
+                                        {!this.props.his && <Button floated='right' onClick={() => this.props.del(this.props.reservation.id)}>Delete reservation</Button>}
+
+                                    </div>}
+
+                                    {this.props.his && <Button floated='right' onClick={() => this.props.res(this.props.reservation.items)}>Renew reservation</Button>}
                                 </div>}
-                                {this.props.his && <Button floated='right' onClick={() => this.props.res(this.props.reservation.items)}>Renew reservation</Button>}
+                                {(this.props.reservation.status.id === 2) && <Label pointing  >Reservation was not approved by administrator.</Label>}
                             </Item.Content>
                         </Item>
                     </Item.Group>}
